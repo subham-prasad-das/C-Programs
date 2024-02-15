b3885b84056d007081baeacea7f1110ec05a0757
subham <dark.demon.das@gmail.com>
2024-02-15 18:25:06 +0530

    Practiseing C Programs

--- LeapYear.c
+++ LeapYear.c
@@ -3,7 +3,7 @@
 int main(){
 
     int year; scanf("%d", &year);
-    if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) printf("It is leap");
+    if((year % 4 == 0) && ((year % 400 == 0) || (year % 100 != 0))) printf("It is leap");
     else printf("It is not leap");
 
     return 0;
