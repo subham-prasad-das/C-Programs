22a4859c18de9b5cff3a84c32fdea3dc3e5667fd
subham <dark.demon.das@gmail.com>
2024-02-16 19:51:40 +0530

    Programming in C

--- Move_0_toRight.c
+++ Move_0_toRight.c
@@ -2,23 +2,29 @@
 
 int main(){
 
-    int arr[] = {0, 5, 6, 2, 0, 3, 0, 0, 5};
-    int n = sizeof(arr) / sizeof(arr[0]);
-    int s = 0, e = n - 1;
-    while(s < e){
-        if(arr[s] == 0) s++;
-        else if(arr[e] != 0) e--;
-        else if(arr[s] != 0 && arr[e] == 0){
+    int arr[10] = {0,0,3,0,5,6,7,0,9,0};
+    int size = sizeof(arr) / sizeof(arr[0]);
+    int s = 0, e = size - 1;
+
+    while(s <= e){
+        if(arr[e] == 0) e--;
+        else if(arr[s] == 0) {
             int temp;
             temp = arr[s];
             arr[s] = arr[e];
             arr[e] = temp;
-            s++; e--;
+            e--;
+            s++;
+        }
+        else if((arr[s] != 0) && (arr[e] != 0)){ 
+            s++;
         }
     }
-    for(int i = 0; i < n; i++){
-        printf("%d", arr[i]);
+
+    for(int i = 0; i < size; i++){
+        printf("%d ", arr[i]);
     }
 
     return 0;
+
 }
