54f67579327731f25cac5a653f2133c3714dedce
subham <dark.demon.das@gmail.com>
2024-02-17 19:24:06 +0530

    Programming in C

--- BinaryToDecimal.c
+++ BinaryToDecimal.c
@@ -2,15 +2,14 @@
 
 int main(){
 
-    int num, a[4], rem, i = 0; 
+    int num, a[4], i = 0; 
     scanf("%d", &num);
     
     if(num == 0) printf("%d",0);
     else if(num == 1) printf("%d",1);
     else{
         while(num){
-            rem = num % 2;
-            a[i] = rem;
+            a[i] = num % 2;
             num /= 2;
             i++;
         }
